efd72b5b08baaf8f09448687cc93748cffdada9b
kanoe <noe.ngongo20@gmail.com>
2019-05-01 14:50:41 +0200

    refactor + q&a

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-$("q1").click(function(){
-    if ( clicked == $("q1b")) {
-        $(this).css('background-color', 'green');
-    } else {
-        $(this).css('background-color', 'red')
-    }
+$( document ).ready(function() {
+    $(".q1r").click(function(e){
+        if ($(".q1").data("charger" , "enabled") == 0){
+            $(".q1".data("charger"));
+        }
+        else 
+        $(".q1").data("charger" , "enabled");
+            if ( e.target.textContent == "B:Tesla" ){
+            $(this).css('background-color', 'green');
+        } else {
+            $(this).css('background-color', 'red');
+        }
+    });
 });
