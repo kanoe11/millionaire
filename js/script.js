1ddd1e45e0417fa1e3af3373ca611b1756fd094f
kanoe <noe.ngongo20@gmail.com>
2019-05-01 19:02:27 +0200

    refactoring + q2

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
 $( document ).ready(function() {
+    $(".question-2").hide();
     $(".q1r").click(function(e){
-        if ($(".q1").data("charger" , "enabled") == 0){
-            $(".q1".data("charger"));
-        }
-        else 
-        $(".q1").data("charger" , "enabled");
+        // verifier l'etat si l'etat  0 le user n'a jamais clique 
+        if ($(".q1").data("enabled") == 0 ){
             if ( e.target.textContent == "B:Tesla" ){
+                $(this).css('background-color', 'green');
+                $(".question-2").show();
+            }  else {
+                $(this).css('background-color', 'red');
+            }
+            $(".q1").data("enabled" , "1");
+        }
+   });
+   $(".q2r").click(function(e){
+    // verifier l'etat si l'etat  0 le user n'a jamais clique 
+    if ($(".q1").data("enabled") == true ){
+        if ( e.target.textContent == "D:Grèce" ){
             $(this).css('background-color', 'green');
-        } else {
-            $(this).css('background-color', 'red');
+            } else {
+                $(this).css('background-color', 'red');
+            }
+            $(".q1").data("enabled" , "false");
         }
     });
 });
